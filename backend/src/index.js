07b741dea7da2db1f16f5e185b9ce0814795f20b
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-02-23 13:48:11 +0000

    feat: add mongoose integration and user model with database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,17 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes.js"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js"
 
-
+dotenv.config()
 const app  = express()
 
 app.use("/api/auth", authRoutes)
 
+const PORT = process.env.PORT
 
 app.listen(5001, () => {
-    console.log("Server Started on port 5001");
+    console.log("Server Started on port: " + PORT);
+    connectDB()
     
 })
