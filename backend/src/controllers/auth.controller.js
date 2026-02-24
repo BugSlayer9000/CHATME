10946bbe93a7d3719bef7fd16f5b1e6966037227
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-02-24 15:54:27 +0000

    feat: enhance login functionality with error handling and JWT generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -51,8 +51,33 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentioals" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    genarateToken(user._id);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullname,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(500).json({message:"Internal server error"})
+  }
 };
 
 export const logout = (req, res) => {
