9521f328d020914801564f70ee109907db9639c2
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-03-09 16:08:34 +0000

    feat: update frontend dependencies and integrate Tailwind CSS
    - Added react-hot-toast and react-router-dom to dependencies.
    - Introduced Tailwind CSS for styling by removing App.css and updating index.css.
    - Created tailwind.config.js for Tailwind configuration.
    - Simplified App.jsx to display a basic "Hello world" message.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",                 // the template
+    "./src/**/*.{js,jsx,ts,tsx}",   // look in JS/JSX/TS/TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
